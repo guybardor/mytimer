89c9a5550f6339ab77e258a6c9f134d5ce54e4d3
guybardor <1993gbd@gmail.com>
2023-05-15 10:11:55 +0300

    second commit
    add all directory

---
+++ mytimer/mytimer/windows/mytimer/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::mytimer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::mytimer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
